c1c1ef7e5d0a27188810714d1ae862ce494c34d0
hugb <553834866@qq.com>
2017-09-09 20:56:56 +0800

    add SSM surport

--- ShiroStudy/src/main/java/com/yummy/controller/EmployeeController.java
+++ ShiroStudy/src/main/java/com/yummy/controller/EmployeeController.java
@@ -3,21 +3,20 @@ package com.yummy.controller;
 import com.yummy.domin.T_tbp_employee;
 import com.yummy.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by yummy on 2017/9/8.
  */
-@RestController
+@Controller
 @RequestMapping("/employee")
 public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
-    @RequestMapping("/getEmployeeById")
-    public T_tbp_employee getEmployeeById(@PathVariable String id){
+    @RequestMapping(value="/getEmployeeById", method = RequestMethod.GET)
+    public T_tbp_employee getEmployeeById(@RequestParam String id){
       return   employeeService.findById(id);
     }
 }
